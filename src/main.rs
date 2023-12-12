74651128b034424ee5abfb3b7fbc861e52fbf36c
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 18:36:32 +1300

    make numeric operation function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
-    /* mut variable
+    /* Mut variable
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
     */
+    /* Variable shadowing
     let x = 5;
     let x = x+1;
     {
@@ -12,4 +13,22 @@ fn main() {
         println!("The value of x in the inner scope is: {x}");
     }
     println!("The value of x is: {x}");
+    */
+    // addition
+    let sum: i8 = 5+10;
+    // subtraction
+    let difference = 95.5-4.3;
+    // multiplication
+    let product = 4*30;
+    // division
+    let quotient = 56.7/32.2;
+    let truncated = -5/3;
+    // remainder
+    let remainder = 43%5;
+    println!("Sum: {sum}");
+    println!("Difference: {difference}");
+    println!("Product: {product}");
+    println!("Quotient: {quotient}");
+    println!("Truncated: {truncated}");
+    println!("Remainder: {remainder}");
 }
