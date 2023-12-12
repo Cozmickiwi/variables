116afddd3e54f9599edb3348a9e61f712fc0f66f
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 19:25:29 +1300

    create tuple function

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,18 @@ fn main() {
     println!("This statement is both {t} and {f}!");
     */
 
+    /* Character 
     let h = 'H';
     let e: char = 'i';
     let happi_cat = '😸';
     println!("{h}{e}{happi_cat}");
+    */
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {y}");
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+    println!("{five_hundred}|{six_point_four}|{one}");
 }
